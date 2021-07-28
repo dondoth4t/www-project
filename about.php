6c5bab8bce58de2ae202ac3420c363cd8439f69f
Zito Namuro <45439075+pottozito@users.noreply.github.com>
2021-07-28 13:13:38 -0400

    Clean up about and contact

--- about.php
+++ about.php
@@ -2,126 +2,119 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <title>COMP 3340 Project</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <title>COMP 3340 Project</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
-    <?php
-    session_start();
-    ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Project</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/comp3340/project">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/comp3340/project/browse.php">Browse</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/comp3340/project/contact.php">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/comp3340/project/about.php">About Us</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
-                            <?php
-                            if (isset($_SESSION['email'])) {
-                                echo $_SESSION['email'];
-                            } else {
-                                echo "Account";
-                            }
-                            ?>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <?php
-                            if (isset($_SESSION['email'])) {
-                                echo '<li><a class="dropdown-item" href="/comp3340/project/account.php">Account</a></li>';
-                                echo '<li><a class="dropdown-item" href="/comp3340/project/logout.php">Log Out</a></li>';
-                            } else {
-                                echo '<li><a class="dropdown-item" href="/comp3340/project/login.php">Log In</a></li>';
-                                echo '<li><a class="dropdown-item" href="/comp3340/project/signup.php">Sign Up</a></li>';
-                            }
-                            ?>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/comp3340/project/cart.php">Cart</a>
-                    </li>
-                </ul>
-            </div>
+  <?php
+  session_start();
+  ?>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Project</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/comp3340/project">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/comp3340/project/browse.php">Browse</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/comp3340/project/contact.php">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/comp3340/project/about.php">About Us</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
+              <?php
+              if (isset($_SESSION['email'])) {
+                echo $_SESSION['email'];
+              } else {
+                echo "Account";
+              }
+              ?>
+            </a>
+            <ul class="dropdown-menu">
+              <?php
+              if (isset($_SESSION['email'])) {
+                echo '<li><a class="dropdown-item" href="/comp3340/project/account.php">Account</a></li>';
+                echo '<li><a class="dropdown-item" href="/comp3340/project/logout.php">Log Out</a></li>';
+              } else {
+                echo '<li><a class="dropdown-item" href="/comp3340/project/login.php">Log In</a></li>';
+                echo '<li><a class="dropdown-item" href="/comp3340/project/signup.php">Sign Up</a></li>';
+              }
+              ?>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/comp3340/project/cart.php">Cart</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <div>
+      <h1>About Us Page</h1>
+      <p>Some text about who we are and what we do.</p>
+      <h2 style="text-align:center">Our Team</h2>
+    </div>
+    <div class="row">
+      <div class="col-12 col-lg-6 mb-4">
+        <div class="card">
+          <img src="zito.jpg" alt="Zito" style="width:100%">
+          <div class="container">
+            <h2>Zito Namuro</h2>
+            <p class="title">CEO & Founder</p>
+            <p>about urself.</p>
+            <p>zito@example.com</p>
+          </div>
         </div>
-    </nav>
-<div class="about-section">
-  <h1>About Us Page</h1>
-  <p>Some text about who we are and what we do.</p>
-</div>
-
-<h2 style="text-align:center">Our Team</h2>
-<div class="row">
-  <div class="column">
-    <div class="card">
-      <img src="zito.jpg" alt="Zito" style="width:100%">
-      <div class="container">
-        <h2>Zito Namuru</h2>
-        <p class="title">CEO & Founder</p>
-        <p>about urself.</p>
-        <p>zito@example.com</p>
-        
+      </div>
+      <div class="col-12 col-lg-6 mb-4">
+        <div class="card">
+          <img src="tyler.jpg" alt="Tyler" style="width:100%">
+          <div class="container">
+            <h2>Tyler Hong</h2>
+            <p class="title">CEO & Founder </p>
+            <p>about urself.</p>
+            <p>tyler@example.com</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6 mb-4">
+        <div class="card">
+          <img src="Karim.jpg" alt="Karim" style="width:100%">
+          <div class="container">
+            <h2>Karim Chahine</h2>
+            <p class="title">CEO & Founder </p>
+            <p>about urself.</p>
+            <p>karimchahine2@hotmail.com</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6 mb-4">
+        <div class="card">
+          <img src="Don.jpg" alt="Don" style="width:100%">
+          <div class="container">
+            <h2>Don</h2>
+            <p class="title">CEO & Founder </p>
+            <p>about urself.</p>
+            <p>don@hotmail.com</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-
-  <div class="column">
-    <div class="card">
-      <img src="tyler.jpg" alt="Tyler" style="width:100%">
-      <div class="container">
-        <h2>Tyler Hong</h2>
-        <p class="title">CEO & Founder </p>
-        <p>about urself.</p>
-        <p>tyler@example.com</p>
-     
-      </div>
-    </div>
-  </div>
-  
-  <div class="column">
-    <div class="card">
-      <img src="Karim.jpg" alt="Karim" style="width:100%">
-      <div class="container">
-        <h2>Karim Chahine</h2>
-        <p class="title">CEO & Founder </p>
-        <p>about urself.</p>
-        <p>karimchahine2@hotmail.com</p>
-   
-      </div>
-    </div>
-  </div>
-</div>
-
- <div class="column">
-    <div class="card">
-      <img src="Don.jpg" alt="Don" style="width:100%">
-      <div class="container">
-        <h2>Don</h2>
-        <p class="title">CEO & Founder </p>
-        <p>about urself.</p>
-        <p>don@hotmail.com</p>
-      
-      </div>
-    </div>
-  </div>
-</div>
-
 </body>
+
 </html>
--- contact.php
+++ contact.php
@@ -5,12 +5,6 @@
     <meta charset="UTF-8">
     <title>COMP 3340 Project</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <style>
-    .center {
-  text-align: center;
-  border: 1px solid black;
-}
-  </style>
 </head>
 
 <body>
@@ -68,45 +62,47 @@
             </div>
         </div>
     </nav>
-    <!--Main heading for contact page-->
-    <div class="center">
-        <h1 class="se-heading text--"> Shoe Factory</h1>
-        <p>
-            <strong> Shoe Factory Locations</strong>
-            <br>401 Sunset Ave, Windsor ON, N9B 3P4
-            <br>Phone:(519)-253-3000
-            <br>Hours: 9a.m.-5.pm. Everyday
-        </p>
-    </div>
-    <br>
-    <div class="center">
-    <h2>Contact Form</h2>
-    <form id="ContactForm" action="mailto:tyler.hong@hotmail.com" method="post">
-        <div>
-            <div class="wrapper">
-                Name:
-                <input type="text" class="input" required>
-            </div>
-            <div class="wrapper">
-                Phone:
-                <input type="text" class="input">
-            </div>
-            <div class="wrapper">
-                Email:
-                <input type="email" class="input" required>
-            </div>
-            <div class="textarea_box">
-                Comments:</br>
-                <textarea name="textarea" cols="20" rows="10" required></textarea>
-
-            </div>
-            <button type="submit" value="submit" class="button2">Send</button>
-            <button type="reset" value="reset" class="button2">Clear</button>
+    <div class="container text-center">
+        <div class="center">
+            <h1 class="se-heading text--"> Shoe Factory</h1>
+            <p>
+                <strong> Shoe Factory Locations</strong>
+                <br>401 Sunset Ave, Windsor ON, N9B 3P4
+                <br>Phone:(519)-253-3000
+                <br>Hours: 9a.m.-5.pm. Everyday
+            </p>
         </div>
-    </form>
+        <br>
+        <div class="center">
+            <h2>Contact Form</h2>
+            <form id="ContactForm" action="mailto:tyler.hong@hotmail.com" method="post">
+                <div>
+                    <div class="mb-2">
+                        <label class="form-label">Name</label>
+                        <input type="text" class="form-control" required>
+                    </div>
+                    <div class="mb-2">
+                        <label class="form-label">Phone</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="mb-2">
+                        <label class="form-label">Email</label>
+                        <input type="email" class="form-control" required>
+                    </div>
+                    <div class="textarea_box mb-4">
+                        <label class="form-label">Comments</label><br>
+                        <textarea class="form-control" name="textarea" cols="100" rows="10" required></textarea>
+                    </div>
+                    <button type="submit" value="submit" class="btn btn-success">Send</button>
+                    <button type="reset" value="reset" class="btn btn-danger">Clear</button>
+                </div>
+        </div>
+        <!--Main heading for contact page-->
+        </form>
     </div>
     </div>
     </div>
 
 </body>
+
 </html>
